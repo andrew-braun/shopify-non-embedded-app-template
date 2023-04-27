d22b2642f3221d78b9f13b79d979c047604337fe
andrew-braun <andrew@andrewbraun.dev>
2023-04-27 20:41:07 +0400

    fix: Rename appUrl and redir variables for UpdateURLsQuery to reflect variable names in the shopify-cli version listed in the package.json

--- src/lib/shopifyCli.mjs
+++ src/lib/shopifyCli.mjs
@@ -1,13 +1,13 @@
-import { api, error as kitError, output, session, ui } from "@shopify/cli-kit";
+import { api, error as kitError, output, session, ui } from "@shopify/cli-kit"
 
 export async function selectOrganizationPrompt(organizations) {
 	if (organizations.length === 1) {
-		return organizations[0];
+		return organizations[0]
 	}
 	const orgList = organizations.map((org) => ({
 		name: org.businessName,
 		value: org.id,
-	}));
+	}))
 	const choice = await ui.prompt([
 		{
 			type: "autocomplete",
@@ -15,8 +15,8 @@ export async function selectOrganizationPrompt(organizations) {
 			message: "Which Partners organization is this work for?",
 			choices: orgList,
 		},
-	]);
-	return organizations.find((org) => org.id === choice.id);
+	])
+	return organizations.find((org) => org.id === choice.id)
 }
 
 const NoOrgError = (organizationId) => {
@@ -34,103 +34,103 @@ const NoOrgError = (organizationId) => {
 			"this organization",
 			`https://partner.shopify.com/${organizationId}`
 		)}`,
-	];
+	]
 	return new error.Abort(
 		`No Organization found`,
 		nextSteps
 			.map((content) => `· ${output.stringifyMessage(content)}`)
 			.join("\n")
-	);
-};
+	)
+}
 
 export const InvalidApiKeyError = (apiKey) => {
 	return new kitError.Abort(
 		output.content`Invalid API key: ${apiKey}`,
 		output.content`You can find the API key in the app settings in the Partner Dashboard.`
-	);
-};
+	)
+}
 
 export async function fetchOrganizations(token) {
-	const query = api.graphql.AllOrganizationsQuery;
-	const result = await api.partners.request(query, token);
-	const organizations = result.organizations.nodes;
-	if (organizations.length === 0) throw NoOrgError();
-	return organizations;
+	const query = api.graphql.AllOrganizationsQuery
+	const result = await api.partners.request(query, token)
+	const organizations = result.organizations.nodes
+	if (organizations.length === 0) throw NoOrgError()
+	return organizations
 }
 
 export async function selectOrg(token) {
-	const orgs = await fetchOrganizations(token);
-	const org = await selectOrganizationPrompt(orgs);
-	return org.id;
+	const orgs = await fetchOrganizations(token)
+	const org = await selectOrganizationPrompt(orgs)
+	return org.id
 }
 
 export async function fetchAppFromApiKey(apiKey, token) {
 	const res = await api.partners.request(api.graphql.FindAppQuery, token, {
 		apiKey,
-	});
-	return res.app;
+	})
+	return res.app
 }
 
 export async function updateURLs(apiKey, url) {
-	const token = await session.ensureAuthenticatedPartners();
+	const token = await session.ensureAuthenticatedPartners()
 
 	const variables = {
 		apiKey,
-		applicationUrl: url + "/app",
-		redirectUrlWhitelist: [
+		appUrl: url + "/app",
+		redir: [
 			`${url}/api/auth`,
 			`${url}/api/auth/callback`,
 			`${url}/api/auth/offline`,
 			`${url}/api/auth/offline-callback`,
 		],
-	};
+	}
 
-	const query = api.graphql.UpdateURLsQuery;
-	const result = await api.partners.request(query, token, variables);
+	const query = api.graphql.UpdateURLsQuery
+	const result = await api.partners.request(query, token, variables)
 	if (result.appUpdate.userErrors.length > 0) {
 		const errors = result.appUpdate.userErrors
 			.map((error) => error.message)
-			.join(", ");
-		throw new kitError.Abort(errors);
+			.join(", ")
+		throw new kitError.Abort(errors)
 	}
 }
 
 function buildAppURL(storeFqdn, publicURL) {
-	const hostUrl = `${storeFqdn}/admin`;
-	const hostParam = btoa(hostUrl).replace(/[=]/g, "");
-	return `${publicURL}/app?shop=${storeFqdn}&host=${hostParam}`;
+	const hostUrl = `${storeFqdn}/admin`
+	const hostParam = btoa(hostUrl).replace(/[=]/g, "")
+	return `${publicURL}/app?shop=${storeFqdn}&host=${hostParam}`
 }
 
 export function outputAppURL(updated, storeFqdn, url) {
-	const appURL = buildAppURL(storeFqdn, url);
-	const heading = output.token.heading("App URL");
-	let message = `Once everything's built, your app's shareable link will be:\n${appURL}`;
+	const appURL = buildAppURL(storeFqdn, url)
+	const heading = output.token.heading("App URL")
+	let message = `Once everything's built, your app's shareable link will be:\n${appURL}`
 	if (updated) {
-		message += `\nNote that your app's URL in Shopify Partners will be updated.`;
+		message += `\nNote that your app's URL in Shopify Partners will be updated.`
 	}
 
-	output.info(output.content`\n\n${heading}\n${message}\n`);
+	output.info(output.content`\n\n${heading}\n${message}\n`)
 }
 
 export function removeHttp(url) {
 	if (url.startsWith("https://")) {
-		const https = "https://";
-		return url.slice(https.length);
+		const https = "https://"
+		return url.slice(https.length)
 	}
 
 	if (url.startsWith("http://")) {
-		const http = "http://";
-		return url.slice(http.length);
+		const http = "http://"
+		return url.slice(http.length)
 	}
 
-	return url;
+	return url
 }
 
-let cli = {};
-cli.fetchAppFromApiKey = fetchAppFromApiKey;
-cli.outputAppURL = outputAppURL;
-cli.removeHttp = removeHttp;
-cli.selectOrg = selectOrg;
-cli.updateURLs = updateURLs;
+let cli = {}
+cli.fetchAppFromApiKey = fetchAppFromApiKey
+cli.outputAppURL = outputAppURL
+cli.removeHttp = removeHttp
+cli.selectOrg = selectOrg
+cli.updateURLs = updateURLs
 
-export default cli;
+export default cli
